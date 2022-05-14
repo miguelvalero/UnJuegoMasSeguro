3fbb17564c4b7409101daf76a9b1e98f64fdc129
miguelvalero <miguel.valero@upc.edu>
2022-05-14 08:31:09 +0200

    Un juego más seguro

---
+++ GeneradorClaves/GeneradorClaves/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace GeneradorClaves
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generar = new System.Windows.Forms.Button();
+            this.PBox = new System.Windows.Forms.TextBox();
+            this.QBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabla = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fraseBox = new System.Windows.Forms.TextBox();
+            this.fraseEncriptadaLbl = new System.Windows.Forms.Label();
+            this.fraseRecuperadaLbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numeroBox = new System.Windows.Forms.TextBox();
+            this.numeroRecuperadoLbl = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.numeroEncriptadoLbl = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // generar
+            // 
+            this.generar.Location = new System.Drawing.Point(122, 68);
+            this.generar.Name = "generar";
+            this.generar.Size = new System.Drawing.Size(110, 23);
+            this.generar.TabIndex = 0;
+            this.generar.Text = "Generar claves";
+            this.generar.UseVisualStyleBackColor = true;
+            this.generar.Click += new System.EventHandler(this.generar_Click);
+            // 
+            // PBox
+            // 
+            this.PBox.Location = new System.Drawing.Point(59, 52);
+            this.PBox.Name = "PBox";
+            this.PBox.Size = new System.Drawing.Size(39, 20);
+            this.PBox.TabIndex = 1;
+            // 
+            // QBox
+            // 
+            this.QBox.Location = new System.Drawing.Point(59, 78);
+            this.QBox.Name = "QBox";
+            this.QBox.Size = new System.Drawing.Size(39, 20);
+            this.QBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "P";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Escribe dos números primos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Q";
+            // 
+            // tabla
+            // 
+            this.tabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tabla.Location = new System.Drawing.Point(59, 112);
+            this.tabla.Name = "tabla";
+            this.tabla.RowHeadersVisible = false;
+            this.tabla.Size = new System.Drawing.Size(240, 393);
+            this.tabla.TabIndex = 6;
+            this.tabla.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tabla_CellClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Texto recuperado con clave (N,d)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Texto encriptado con clave (N,e)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Texto original";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Test frase";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fraseBox
+            // 
+            this.fraseBox.Location = new System.Drawing.Point(201, 66);
+            this.fraseBox.Name = "fraseBox";
+            this.fraseBox.Size = new System.Drawing.Size(240, 20);
+            this.fraseBox.TabIndex = 13;
+            // 
+            // fraseEncriptadaLbl
+            // 
+            this.fraseEncriptadaLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fraseEncriptadaLbl.Location = new System.Drawing.Point(201, 99);
+            this.fraseEncriptadaLbl.Name = "fraseEncriptadaLbl";
+            this.fraseEncriptadaLbl.Size = new System.Drawing.Size(240, 32);
+            this.fraseEncriptadaLbl.TabIndex = 14;
+            // 
+            // fraseRecuperadaLbl
+            // 
+            this.fraseRecuperadaLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fraseRecuperadaLbl.Location = new System.Drawing.Point(201, 143);
+            this.fraseRecuperadaLbl.Name = "fraseRecuperadaLbl";
+            this.fraseRecuperadaLbl.Size = new System.Drawing.Size(240, 31);
+            this.fraseRecuperadaLbl.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.fraseBox);
+            this.groupBox1.Controls.Add(this.fraseRecuperadaLbl);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.fraseEncriptadaLbl);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(376, 222);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(565, 195);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Encriptación de frases";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numeroBox);
+            this.groupBox2.Controls.Add(this.numeroRecuperadoLbl);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.numeroEncriptadoLbl);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(376, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(401, 195);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Encriptación de números";
+            // 
+            // numeroBox
+            // 
+            this.numeroBox.Location = new System.Drawing.Point(214, 40);
+            this.numeroBox.Name = "numeroBox";
+            this.numeroBox.Size = new System.Drawing.Size(54, 20);
+            this.numeroBox.TabIndex = 13;
+            // 
+            // numeroRecuperadoLbl
+            // 
+            this.numeroRecuperadoLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numeroRecuperadoLbl.Location = new System.Drawing.Point(214, 100);
+            this.numeroRecuperadoLbl.Name = "numeroRecuperadoLbl";
+            this.numeroRecuperadoLbl.Size = new System.Drawing.Size(54, 31);
+            this.numeroRecuperadoLbl.TabIndex = 15;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(23, 100);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(173, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Número recuperado cin cleve (N,d)";
+            // 
+            // numeroEncriptadoLbl
+            // 
+            this.numeroEncriptadoLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numeroEncriptadoLbl.Location = new System.Drawing.Point(214, 65);
+            this.numeroEncriptadoLbl.Name = "numeroEncriptadoLbl";
+            this.numeroEncriptadoLbl.Size = new System.Drawing.Size(54, 32);
+            this.numeroEncriptadoLbl.TabIndex = 14;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(23, 68);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(173, 13);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Número encriptado con cleve (N,e)";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(120, 40);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(80, 13);
+            this.label12.TabIndex = 9;
+            this.label12.Text = "Número original";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(294, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Test número";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(238, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 712);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabla);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.QBox);
+            this.Controls.Add(this.PBox);
+            this.Controls.Add(this.generar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tabla)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generar;
+        private System.Windows.Forms.TextBox PBox;
+        private System.Windows.Forms.TextBox QBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView tabla;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox fraseBox;
+        private System.Windows.Forms.Label fraseEncriptadaLbl;
+        private System.Windows.Forms.Label fraseRecuperadaLbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox numeroBox;
+        private System.Windows.Forms.Label numeroRecuperadoLbl;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label numeroEncriptadoLbl;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ GeneradorClaves/GeneradorClaves/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace GeneradorClaves
+{
+    public partial class Form1 : Form
+    {
+        int N, clave_e, clave_d;
+
+
+        private int PotenciaModulo(int numero, int N, int c)
+        // Calcula numero elevado a c módulo N
+        // Lo hago de esta manera para evitar que el número que se va generando 
+        // sea muy grande y desborde la capacidad de las variables
+        {
+            int t = 1;
+            for (int i = 0; i < c; ++i)
+                t = t * numero % N; // asi el resultado parcial se mantiene pequeño
+            return t;
+        }
+
+
+        private string Encriptar(string mensaje, int N, int c)
+        // Cada caracter del mensaje tiene un códugo ASCII que va entre el 32 
+        // (correspondiente al espacio en blando) y el 126 (que corresponde a la tilde)
+        // Esos son los 94 caracteres imprimibles (consulta la tabla ASCII en Google).
+        // Al código de cada caracter le restamos 32 (el código del primero de los imprimibles
+        // Asi obtenemos un número entre el 0 y el 94. Ese es el número al que aplicamos
+        // la fórmula de encriptación y al resultado de volvemos a sumar 32 para obtener el código
+        // de otro de los caracteres imprimibles, que es el que enviaremos en el mensaje codificado
+        // Para que esto funcione es necesario que N sea mayor que 94
+
+        {
+            int tam = mensaje.Length;
+            char[] caracteres = mensaje.ToCharArray();
+            char[] resultado = new char[tam];
+            for (int i = 0; i < tam; i++)
+            {
+                int codigo = caracteres[i] - Convert.ToInt32(' '); 
+                int res = PotenciaModulo(codigo, N, c);
+                resultado[i] = Convert.ToChar(res + Convert.ToInt32(' '));
+            }
+           
+
+            return new string(resultado);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Boolean SonPrimosRelativos(int x, int y)
+        {
+            if (y == 0)
+                if (x == 1)
+                    return true;
+                else
+                    return false;
+            else
+                return SonPrimosRelativos(y, x % y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int numero = Convert.ToInt32(numeroBox.Text);
+            if (numero >= N)
+                MessageBox.Show("El número a encriptar debe ser menor que N");
+            else
+            {
+                int encriptado = PotenciaModulo(numero, N, clave_e);
+                numeroEncriptadoLbl.Text = encriptado.ToString();
+                int recuperado = PotenciaModulo(encriptado, N, clave_d);
+                numeroRecuperadoLbl.Text = recuperado.ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (N < 94)
+                MessageBox.Show("N debe ser mayor de 94");
+            else { 
+                string fraseEnciptada = Encriptar(fraseBox.Text, N, clave_e);
+                fraseEncriptadaLbl.Text = fraseEnciptada;
+                string fraseRecuperada = Encriptar(fraseEnciptada, N, clave_d);
+                fraseRecuperadaLbl.Text = fraseRecuperada;
+            }
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            tabla.Rows.Clear();
+        }
+
+        private void generar_Click(object sender, EventArgs e)
+        // Aquí es donde generamos todas las posibles claves asociadas a los dos
+        // números primos elegidos
+        // El código es sencillo y debería entenderse fácilmente
+        {
+            int P = Convert.ToInt32(PBox.Text);
+            int Q = Convert.ToInt32(QBox.Text);
+            int N = P * Q;
+            int PHI_N = (P - 1) * (Q - 1);
+
+            List<int> posiblesValoresPara_e = new List<int>();
+            for (int i = 1; i < PHI_N; i++)
+                if (SonPrimosRelativos(i, PHI_N))
+                    posiblesValoresPara_e.Add(i);
+
+            List<Tuple<int, int, int>> claves = new List<Tuple<int, int, int>>();
+
+            foreach (int i in posiblesValoresPara_e)
+                for (int d = 1; d < PHI_N; d++)
+                    if (((d * i) % PHI_N == 1) & (i < d))
+                    {
+                        claves.Add(new Tuple<int, int, int>(N, i, d));
+                        break;
+                    }
+
+            tabla.ColumnCount = 3;
+            tabla.ColumnHeadersVisible = true;
+
+
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+            columnHeaderStyle.BackColor = Color.Beige;
+            columnHeaderStyle.Font = new Font("Verdana", 10, FontStyle.Bold);
+            tabla.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+
+            tabla.Columns[0].Name = "N";
+            tabla.Columns[0].Width = 50;
+            tabla.Columns[1].Name = "e";
+            tabla.Columns[1].Width = 50;
+            tabla.Columns[2].Name = "d";
+            tabla.Columns[2].Width = 50;
+            foreach (Tuple<int, int, int> t in claves)
+                tabla.Rows.Add(t.Item1, t.Item2, t.Item3);
+
+            tabla.ScrollBars = ScrollBars.None;
+
+            // Ajusto el tamaño de la tabla a su contenido
+
+            int height = 0;
+            foreach (DataGridViewRow row in tabla.Rows)
+                height += row.Height;
+            
+            height += tabla.ColumnHeadersHeight;
+
+            int width = 0;
+            foreach (DataGridViewColumn col in tabla.Columns)
+                width += col.Width;
+  
+
+            tabla.ClientSize = new Size(width + 2, height + 2);
+
+            tabla.ClearSelection();
+        }
+
+
+        private void tabla_CellClick(object sender, DataGridViewCellEventArgs e)
+        // si clico en una fila de la tabla, selecciono los valores de esa fila
+        // y la marco el rojo
+        {
+            int fila = e.RowIndex;
+            // Primero pongo todas las filas en blanco
+            foreach (DataGridViewRow row in tabla.Rows)
+              row.DefaultCellStyle.BackColor = Color.White;
+            N = Convert.ToInt32(tabla.Rows[fila].Cells[0].Value);
+            clave_e = Convert.ToInt32(tabla.Rows[fila].Cells[1].Value);
+            clave_d = Convert.ToInt32(tabla.Rows[fila].Cells[2].Value);
+            tabla.Rows[fila].DefaultCellStyle.BackColor = Color.Red;
+            tabla.ClearSelection();
+
+        }
+    }
+}
